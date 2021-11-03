0777b82f2c9b95394d3bcabbbc7bbe0c4cd92602
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-03 17:44:16 +0200

    Create basic game board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useState } from "react";
+import "./App.css";
+
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
+
+const getRandomColor = () =>
+  candyColors[Math.floor(Math.random() * candyColors.length)];
+
+const createBoard = () => new Array(width * width).fill().map(getRandomColor);
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState(() =>
+    createBoard()
+  );
+
+  return (
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={candyColor}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
